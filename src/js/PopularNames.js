5752a013ccc2b05175a796e089a33c60a871312c
David-Rodden <davidrodden273@gmail.com>
2017-03-15 22:04:17 -0700

    Will instead provide data to display on site instead of pulling from other sites

--- src/js/PopularNames.js
+++ src/js/PopularNames.js
@@ -6,11 +6,23 @@ var baseUrl = "http://www.studentsoftheworld.info/penpals/stats.php3?Pays=";
 var countries = ["ESP", "FRA"];
 
 countries.forEach(function (element) {
-   $.ajax({
-       url: href,
-       type: 'GET',
-       success: function (data) {
-           console.log(data)
-       }
-   })
+    $.ajax({
+        url: href,
+        type: 'GET',
+        success: function (data) {
+            console.log(data)
+        }
+    })
 });
+
+function readFile(file, out) {
+    var http = new XMLHttpRequest();
+    http.open('get', file);
+    http.onreadystatechange = function () {
+        out.innerHTML = http.responseText.replace(/\n/g, '<br>');
+    };
+    http.send();
+}
+function readInput() {
+    readFile('test.txt', document.getElementById('text-file'));
+}
